314d8761119ecce72d1040682152ae5c172a8586
angelicaosuna-dev <angelicaosuna@mail.weber.edu>
2025-10-16 20:25:03 -0600

    actividad 3-5

--- M8_presencial/gestor.py
+++ M8_presencial/gestor.py
@@ -27,6 +27,35 @@ def main():
             for i in range(3):
                 nota = float(input(f"Ingrese la nota {i+1}: "))
                 notas.append(nota)
+                try: #3
+                    nota = float(input(f"Ingrese la nota {i+1} (0-10): "))
+                    if 0 <= nota <= 10:
+                        notas.append(nota)
+                        nota_valida = True
+                    else:
+                        print("error: ingresa el numero valido.")
+                except ValueError:
+                    print("ingresa el numero valido.")
+                print(f"  Primeras dos notas: {est['notas'][:2]}") #4
+        elif opcion == "5": #5
+            if len(estudiantes) == 0:
+                print("\n no hay estudiante registrado.")
+            else:
+                nombre_buscar = input("ingresa el nombre del estudiante buscado:")
+                encontrado = False
+                for est in estudiantes:
+                    if est["nombre"].lower() == nombre_buscar.lower():
+                        encontrado = True
+                        print("\n" + "=" * 40 )
+                        print(f"estudiantes: {est['nombre']}")
+                        print(f"notas: {est['notas']}")
+                        print(f"promedio: {est['promedio']:.2f}")
+                        estado = "aprobado" if est["aprobado"] else "reprobado"
+                        print(f"estado: {estado}")
+                        print("=" * 40)
+                        break
+                if not encontrado:
+                    print(f"nose encontro un estudiantes con ese nombre '{nombre_buscar}'.")
 
             promedio = calcular_promedio(notas)
             aprobado = promedio >= 6.0
